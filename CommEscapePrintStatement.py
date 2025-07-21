d1b4b4ea48f143be192d446106387f54cbbb4ecc
Rishi <189157642+xrishis@users.noreply.github.com>
2025-07-21 16:47:49 +0530

    Create CommEscapePrintStatement.py

---
+++ CommEscapePrintStatement.py
@@ -0,0 +1,32 @@
+#Comments - 
+
+#ThisisRishi
+
+Print("Hey this is Rishi /nwhere are you")
+
+Triple Single quote , and Triple Double quote is also used to give comments.
+
+Escape Sequences Characters - 
+
+\n - New Line 
+
+\ - Backslash character used to bypass double quoted lines in a print statement.
+
+
+example - print("Hey this is \"Rishi" here")
+
+Parameters -------
+
+° sep="~"
+
+print("Hey",6,7, sep="~")
+
+
+° end="009"
+
+print("heyyyyyyy", End="null")
+
+
+print("heyyyyyyy", End="009")
+
+
